a4450709da8116ba113947cda5251f00107f4446
Esparta Vidal <vidalisabel5@hotmail.com>
2016-10-26 14:21:32 -0200

    open image

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 
-print("Hello World")
+print("Hello Migas")
+import cv2
 
+# Reading images
 
-
+dish = cv2.imread('images/dish0.jpg')
+cv2.imshow('dish', dish)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
